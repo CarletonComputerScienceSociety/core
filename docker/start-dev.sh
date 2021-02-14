ed87a95838125fe36e0d90b8ce63429d260f2f2b
MathyouMB <matthewmacraebovell@bell.net>
2021-02-14 04:11:25 -0500

    fix compose config

--- docker/start-dev.sh
+++ docker/start-dev.sh
@@ -3,3 +3,4 @@ export DJANGO_SETTINGS_MODULE=core.settings.dev
 python manage.py migrate
 python manage.py collectstatic --noinput
 python manage.py runserver 0.0.0.0:8000
+celery -A core worker -l info
--- docker/start-prod.sh
+++ docker/start-prod.sh
@@ -3,3 +3,4 @@ export DJANGO_SETTINGS_MODULE=core.settings.prod
 python manage.py migrate
 python manage.py collectstatic --noinput
 python manage.py runserver 0.0.0.0:8000
+celery -A core worker -l info
