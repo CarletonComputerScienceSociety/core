ad6cacfebeb8233f27a79abe2962454cb583cfbe
MathyouMB <matthewmacraebovell@bell.net>
2021-02-13 21:42:29 -0500

    add backups to folder

--- core/tasks.py
+++ core/tasks.py
@@ -2,11 +2,20 @@ from __future__ import absolute_import, unicode_literals
 
 from celery import shared_task
 import sys
+import os
 from django.core.management import call_command
 from django.core import management
 from django.core.management.commands import dumpdata
+from datetime import datetime
+
 
 @shared_task
 def backup():
-    with open('./backup.json', 'w') as f:
+    now = datetime.now()
+    timestamp = datetime.timestamp(now)
+
+    if not os.path.isdir('./backups'):
+        os.mkdir('./backups')
+
+    with open('./backups/'+str(timestamp)+'.json', 'w') as f:
         management.call_command('dumpdata', stdout=f)
