175efb871f742808cf7d28452af38ba0d880a86a
MathyouMB <matthewmacraebovell@bell.net>
2021-04-29 12:30:22 -0400

    lets seee

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -2,7 +2,6 @@ from .base import *
 
 from django.core.management.utils import get_random_secret_key
 
-if os.environ.get("GITHUB_WORKFLOW"):
-    SECRET_KEY = get_random_secret_key()
+SECRET_KEY = get_random_secret_key()
 
 DEBUG = True
