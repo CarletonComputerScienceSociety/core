ef5336937f2e6adf65603d6d998b4133f59ff90e
Forest Anderson <forestkzanderson@gmail.com>
2021-09-09 22:31:09 -0400

    Fixed boot

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -1,7 +1,6 @@
 from django.core.management.utils import get_random_secret_key
 from sentry_sdk.integrations.django import DjangoIntegration
 import sentry_sdk
-import environ
 
 from .base import *
 SECRET_KEY = get_random_secret_key()
