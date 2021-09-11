d2697f75ce9b2d45dd88fc65b085ba56c361754a
Forest Anderson <forestkzanderson@gmail.com>
2021-09-11 15:43:16 -0400

    Testing workflow file (#51)
    * Testing workflow file
    
    * Fixed login and push
    
    * Formatting
    
    * Try different tag
    
    * Cleaned up Docker build
    
    * Changed image name
    
    * Fixed production errors
    
    * Fixed boot
    
    * Fix wsgi location
    
    * Run testing for prod
    
    * Try waiting for database
    
    * Connection message
    
    * Better messages
    
    * Echo waiting for db
    
    * Change db name
    
    * Fixed prod
    
    * Change entrypoints
    
    * Whitenoise
    
    * Only build on master

--- docker/start-prod.sh
+++ docker/start-prod.sh
@@ -4,6 +4,18 @@ export DJANGO_SETTINGS_MODULE=core.settings.prod
 
 cd /code
 
+# Wait for the database to be ready
+echo "Waiting for database"
+python /code/docker/wait.py
+
+echo "Migrating"
 python manage.py migrate
+
+echo "Collecting static files"
 python manage.py collectstatic --noinput
-python manage.py runserver 0.0.0.0:8000
+
+echo "Starting server"
+gunicorn \
+    -w 3 \
+    -b 0.0.0.0:8000 \
+    core.wsgi:application
---
+++ docker/wait.py
@@ -0,0 +1,16 @@
+import psycopg2
+import time
+
+while True:
+    time.sleep(0.5)
+    try:
+        conn = psycopg2.connect(
+            host="localhost",
+            database="code_project",
+            user="postgres",
+            password="1234"
+        )
+        print("Connected to database!")
+        break
+    except:
+        continue
